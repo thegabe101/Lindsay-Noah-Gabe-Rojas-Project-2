19f4d747b0446ab5f969fff92a2790a9e90b840b
LindsayLew <lmschwehr@gmail.com>
2022-08-05 15:47:14 -0700

    burger victory

--- public/js/style.js
+++ public/js/style.js
@@ -1,10 +1,10 @@
-const source = document.querySelector(".goLogin").innerHTML;
-const template = Handlebars.compile(source);
-document.body.innerHTML = template()
+// const source = document.querySelector(".goLogin").innerHTML;
+// const template = Handlebars.compile(source);
+// document.body.innerHTML = template()
 
 // navbar action
 const navBar = () => {
-    let x = document.getElementsByClassName("navLinks");
+    let x = document.querySelector(".navLinks");
     if (x.style.display === "block") {
         x.style.display = "none";
     } else {
@@ -12,6 +12,6 @@ const navBar = () => {
     }
 };
 
-document.querySelector(".goLogin").addEventListener("click",e=>{
-    location.href = "/login"
-})
+// document.querySelector(".goLogin").addEventListener("click",e=>{
+//     location.href = "/login"
+// })
