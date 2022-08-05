94f34143c0dea8fdd5bffa7d2c59e25fe137efaf
LindsayLew <lmschwehr@gmail.com>
2022-08-04 18:46:35 -0700

    configuring custom tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-    
+  // content is where we configure the paths to all of the html templates
+  content: [
+    "./views/**/*.{handlebars}",
+    "./html/**/*.{html}",
+    "./public/**/*.{js}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary:'#ff828f',
+        background: '#ffd166',
+        secondary: '#ffba66',
+        accent: '#ff7497',
+        light: '#ffbad4'
+      },
+    }
+  },
+  plugins: [],
 }
