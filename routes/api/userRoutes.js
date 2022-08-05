4ae0a000bd7c5a2f221dd3eb07c8770e33a9691b
Rayncor <sowa332@gmail.com>
2022-08-05 14:20:53 -0700

    practice branch

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const User   = require("../../models/User");
+const { User } = require("../../models");
 const haveAuth = require('../../utils/auth');
 const bcrypt = require('bcrypt');
 
--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,6 +1,6 @@
 const seedUsers = require('./user-seeds');
 // const seedBooks = require('./book-seeds');
-// const seedCatalogs = require('./catalog-seeds');
+const seedCatalogs = require('./catalog-seeds');
 
 const sequelize = require('../config/connection');
 
@@ -14,8 +14,8 @@ const seedDatabase = async () => {
     // await seedBooks();
     // console.log('\n---------------------- BOOKS SUCCESSFULLY SEEDED ------------------------\n');
 
-    // await seedCatalogs();
-    // console.log('\n-------------------- CATALOGS SUCCESSFULLY SEEDED -------------------\n');
+    await seedCatalogs();
+    console.log('\n-------------------- CATALOGS SUCCESSFULLY SEEDED -------------------\n');
 
     process.exit(0);
 };
