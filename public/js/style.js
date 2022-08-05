d577d7d428d8e214c9b40e55eb2f3621e1028982
LindsayLew <lmschwehr@gmail.com>
2022-08-04 21:06:03 -0700

    test

---
+++ public/js/style.js
@@ -0,0 +1,10 @@
+
+// navbar action
+const navBar = () => {
+    let x = document.getElementsByClassName("navLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+};
