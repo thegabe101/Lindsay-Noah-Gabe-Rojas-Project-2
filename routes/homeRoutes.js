7b38cf93737024b34587a7b86027e00b45b75070
Rayncor <sowa332@gmail.com>
2022-08-05 17:44:08 -0700

    login post new user route working

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -4,18 +4,18 @@ const router = require('express').Router();
 // const sequelize = require('../config/connection');
 const { User } = require('../models');
 // const Book = require('../models/Book'); 
-const { Catalog } = require('../models'); 
+const { Catalog } = require('../models');
 
 //front end routes to the login and home pages. 
 
-router.get("/",(req,res)=>{
+router.get("/", (req, res) => {
     Catalog.findAll({
-        include:[User]
-    }).then(data=>{
-        const hbsData = data.map(cata=>cata.toJSON())
-        res.render("home",{
-            catalogs:hbsData,
-            loggedIn:req.session.loggedIn
+        include: [User]
+    }).then(data => {
+        const hbsData = data.map(cata => cata.toJSON())
+        res.render("home", {
+            catalogs: hbsData,
+            loggedIn: req.session.loggedIn
         })
     })
 })
@@ -26,11 +26,11 @@ router.get('/home', (req, res) => {
         return;
     }
     User.findByPk(req.session.user_id, {
-        include:[Catalog]
-    }).then(userData=>{
+        include: [Catalog]
+    }).then(userData => {
         const hbsData = userData.toJSON();
         console.log(hbsData)
-        hbsData.loggedIn=true;
+        hbsData.loggedIn = true;
         res.render("home", hbsData)
     })
 });
@@ -41,7 +41,7 @@ router.get('/login', (req, res) => {
         return;
     }
 
-    res.render('login',{loggedIn:false});
+    res.render('login', { loggedIn: false });
 });
 
 router.get('/signup', (req, res) => {
