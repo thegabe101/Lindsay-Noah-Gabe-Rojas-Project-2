827bbf80f5e6b6c140bd69ddb42cb2679e1bb190
gio2nice <106710271+gio2nice@users.noreply.github.com>
2022-08-07 12:40:22 -0700

    changed

--- APITest/index.js
+++ APITest/index.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-// var keyData = "0553283685";
-// $.ajax({
-//   url: "https://openlibrary.org/api/books?bibkeys=ISBN:" + keyData + "&jscmd=details&callback=mycallback",
-//   dataType: "jsonp",
-//   success: function(data) {
-//     var getData = data["ISBN:" + keyData];
-//     var title = getData.details.title,
-//       author = getData.details.authors[0].name;
-//     $('.title').text(title);
-//     $('.author').text(author);
-//   }
-// });
-
-// const { response } = require("express");
-
-function getBooks(){
-  document.getElementById('output').innerHTML="";
-  fetch("http://openlibrary.org/search.json?q="+document.getElementById("input").value)
-  .then(a=>a.json())
-  .then(response => {
-    for(var i=0; i<1;i++){
-      document.getElementById("output").innerHTML+="<h2>"+response.docs[i].title+"</h2>"+response.docs[i].author_name[0]+"<br><img src='http://covers.openlibrary.org/b/isbn/"+response.docs[i].isbn[0]+"-M.jpg'><br>";
-    }
-  });
-}
-=======
 // Ditched due to inability to get reliable consistent ISBN upon user input of title name.
 
 // var keyData = "0553283685";
@@ -111,4 +84,3 @@ function getBooks() {
       }
     })
 }
->>>>>>> dev
