ef8917fe25d307b5c8ed0efc68788d1e14bdcf54
Rayncor <sowa332@gmail.com>
2022-08-07 11:39:59 -0700

    working on catalog routes

--- routes/api/catalogRoutes.js
+++ routes/api/catalogRoutes.js
@@ -2,26 +2,60 @@ const router = require('express').Router();
 const { User } = require("../../models");
 const { Catalog } = require("../../models");
 const { Book } = require("../../models");
+const haveAuth = require('../../utils/auth');
 
 router.get('/', (req, res) => {
-    //GMS not sure yet how were going to do this. first going to see if i can retrieve catalogs before worrying about which user they belong to. 
-    //GMS this will require some clarification of our SQL side.
+    console.log('------route hit-------');
     Catalog.findAll({
+        attributes: [
+            'id',
+            'name',
+            'genre_type'
+        ],
+        order: [['created_at', 'DESC']],
+        include: [
+            //GMS we will attach user to book after including the book model that is possessed by the catalogs
+            {
+                model: Book,
+                attributes: ['id', 'title', 'author', 'isbn_num', 'owned'],
+                include: {
+                    model: User,
+                    attributes: ['username', 'email']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username', 'email']
+            },
+        ]
     })
-        .then(catData => res.json(catData))
+        .then(catPostData => res.json(catPostData))
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
         });
 });
 
-router.post('/', (req, res) => {
+// router.get('/', (req, res) => {
+//     //GMS not sure yet how were going to do this. first going to see if i can retrieve catalogs before worrying about which user they belong to. 
+//     //GMS this will require some clarification of our SQL side.
+//     Catalog.findAll({
+//     })
+//         .then(catData => res.json(catData))
+//         .catch(err => {
+//             console.log(err);
+//             res.status(500).json(err);
+//         });
+// });
+
+router.post('/', haveAuth, (req, res) => {
     //GMS going to try creating new route for the post function i am working with on js pub. we are currently able to retrieve but not post new cats
     //GMS we might need to run some sort of logged in check prior to this post but can put that on backburner. this would be to ensure that a new cat posted is only by that user
     Catalog.create({
 
         name: req.body.name,
         genre_type: req.body.genre_type,
+        user_id: req.session.user_id
 
     }).then
         // console.log("am i doing this right??")
