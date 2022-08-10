783e07e6ef4445c0408c674ae57d1e6fc56bad52
gio2nice <106710271+gio2nice@users.noreply.github.com>
2022-08-10 01:25:37 -0700

    still figuring it out

--- models/comment.js
+++ models/comment.js
@@ -1,7 +1,27 @@
-const { Model, DataTypes } = require('sequelize');
-const mySequelize = require('../config/connection');
+const Sequelize = require('sequelize');
+// const mySequelize = require('../config/connection');
 const sequelize = new Sequelize('database', 'username', 'password');
 
-const ModelComments = require('sequelize-model-comments').init(sequelize, options);
+const ModelComments = require('sequelize-model-comments').init(sequelize, options || {});
+ModelComments.defineModels();
 
-// ModelComments.defineModels({});
+const Post = sequelize.define('Post', {
+    title: Sequelize.STRING,
+    content: Sequelize.STRING,
+});
+
+Post.enableModelComments();
+
+const options = {
+    /* ... */
+    userModel: 'users',
+};
+
+// Post.update({
+//     /* ... */
+//     comment: 'This attribute will be used to create a comment which annotates the revision'
+// }, {
+//     userId: user.id
+// }).then(() {
+//     /* ... */
+// });
